121cdf977c49ecc59e79617f5046e725b6c756fb
cebourgeois <cedricbourg@gmail.com>
2016-03-22 17:30:30 +0100

    style

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,9 +3,10 @@ module ApplicationHelper
 #retournons un titre en fonction de la situation
 def titre
 	base_titre= "App du Tutoriel Ruby on Rails"
-	if @titre=nil 
+	if @titre==nil
 		base_titre
 	else
 		"#{base_titre} | #{@titre}"
 	end
+end
 end
